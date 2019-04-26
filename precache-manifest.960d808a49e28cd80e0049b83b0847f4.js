b28f35dce037dc6f24fd9ebb7d730b0f21c744b6
Amar <amar.ravi@outlook.com>
2019-04-27 01:12:12 +0530

    Updates

---
+++ precache-manifest.960d808a49e28cd80e0049b83b0847f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "929224de2070fb8d3963",
+    "url": "/amartainment/fool-me-thrice/static/css/main.1dab6acc.chunk.css"
+  },
+  {
+    "revision": "929224de2070fb8d3963",
+    "url": "/amartainment/fool-me-thrice/static/js/main.8127a228.chunk.js"
+  },
+  {
+    "revision": "48c5647748e07c114eda",
+    "url": "/amartainment/fool-me-thrice/static/js/runtime~main.dae68593.js"
+  },
+  {
+    "revision": "f02fc1198d378bb437f0",
+    "url": "/amartainment/fool-me-thrice/static/js/2.cb2203c2.chunk.js"
+  },
+  {
+    "revision": "82dc68ecea3a8ff67618db622399f2b5",
+    "url": "/amartainment/fool-me-thrice/static/media/dashboard.82dc68ec.svg"
+  },
+  {
+    "revision": "4ef89b7ccf369e61c28976be737a2c3f",
+    "url": "/amartainment/fool-me-thrice/static/media/write.4ef89b7c.svg"
+  },
+  {
+    "revision": "fbf7eb0cb565bb511659731a0e39e64c",
+    "url": "/amartainment/fool-me-thrice/static/media/read.fbf7eb0c.svg"
+  },
+  {
+    "revision": "4f276d09debcccf2e5ceccdf53b3ae9d",
+    "url": "/amartainment/fool-me-thrice/static/media/read-black.4f276d09.svg"
+  },
+  {
+    "revision": "ce453b160a793cff5ba6e322801ffbc1",
+    "url": "/amartainment/fool-me-thrice/static/media/dashboard-black.ce453b16.svg"
+  },
+  {
+    "revision": "82ca0e34796a1dc1e5fae19be7ac9ae8",
+    "url": "/amartainment/fool-me-thrice/static/media/write-black.82ca0e34.svg"
+  },
+  {
+    "revision": "155d682bc0516ba859ca0436ca935e86",
+    "url": "/amartainment/fool-me-thrice/index.html"
+  }
+];
